95e98c1a916f48852cec7d69ae80b5711c10938b
pawelm1984 <>
2023-11-14 13:23:50 +0000

    First commit

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
-# Your 'variables.tf' input variables go here.
+variable "tenancy_ocid" {
+  type        = string
+  description = "Tenancy ID"
+}
+
+variable "dynamic_group_name" {
+  type  = string
+  description = "Name of the dynamic group"
+}
+
+variable "dynamic_group_description" {
+  type  = string
+  description = "Description of the dynamic group"
+}
+
+variable "matching_rules" {
+  type = string
+  description = "What resources are in the dynamic group"
+}
